0c70019ed5b3a1ca88f77cdf7ddda214740a2187
Josh Lovin <76003574+jdlovin7185@users.noreply.github.com>
2021-03-11 15:07:20 -0500

    Adds server request

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import axios from 'axios';
 
 import './login-view.scss';
 
@@ -12,8 +13,18 @@ export function LoginView(props) {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(username, password);
-    props.onLoggedIn(username)
+  //  Send a request to the server for authentication
+  axios.post('https://myflix1-0.herokuapp.com/', {
+    Username: username,
+    Password: password
+  })
+  .then(response => {
+    const data = response.data;
+    props.onLoggedIn(data);
+  })
+  .catch(e => {
+    console.log('no such user')
+    });
   };
 
     return (
