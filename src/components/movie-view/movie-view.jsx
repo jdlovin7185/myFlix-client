af2db7be3d960fb4920f4f58eedea87acc5d200e
Josh Lovin <76003574+jdlovin7185@users.noreply.github.com>
2021-03-15 11:07:52 -0400

    Fixed bug

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import './movie-view.scss';
 
@@ -20,34 +21,41 @@ export class MovieView extends React.Component {
     
     return (
       <div className="movie-view movie-border">
-        <Card>
-          <Card.Img className="movie-poster" src={movie.ImagePath}/>
-          <Card.Title>{movie.Title}</Card.Title>
-          <Card.Body>
-            <Card.Text>{movie.Description}</Card.Text>
-            <Card.Text>Director: {movie.Director.Name}</Card.Text>
-            <Card.Text>Genre: {movie.Genre.Name}</Card.Text>
-          </Card.Body>
-        </Card>
-        <Card.Footer>
-        <Link to={`/director/${movie.Director.Name}`}>
-            <Button variant="link">Director</Button>
-          </Link>
-          <Link to={`/genres/${movie.Genre.Name}`}>
-            <Button variant="link">Genre</Button>
-          </Link>
-        </Card.Footer>
+        <img className="movie-poster" src={movie.ImagePath} />
+        <div className="movie-title">
+          <span className="label">Title: </span>
+          <span className="value">{movie.Title}</span>
+        </div>
+        <div className="movie-description">
+          <span className="label">Description: </span>
+          <span className="value">{movie.Description}</span>
+        </div>
+        <div className="movie-genre">
+          <span className="label">Genre: </span>
+          <span className="value">{movie.Genre.Name}</span>
+        </div>
+        <div className="movie-director">
+          <span className="label">Director: </span>
+          <span className="value">{movie.Director.Name}</span>
+        </div>
+        <Link to={`/directors/${movie.Director.Name}`}>
+          <Button variant="link">Director</Button>
+        </Link>
+
+        <Link to={`/genres/${movie.Genre.Name}`}>
+          <Button variant="link">Genre</Button>
+        </Link>
       </div>
     );
   }
 }
 
-MovieView.propTypes = {
-  movie: PropTypes.shape({
-    ImagePath: PropTypes.string.isRequired,
-    Title: PropTypes.string.isRequired,
-    Description: PropTypes.string.isRequired,
-    Genre: PropTypes.string.isRequired,
-    Director: PropTypes.string.isRequired
-  })
-}
+// MovieView.propTypes = {
+//   movie: PropTypes.shape({
+//     ImagePath: PropTypes.string.isRequired,
+//     Title: PropTypes.string.isRequired,
+//     Description: PropTypes.string.isRequired,
+//     Genre: PropTypes.string.isRequired,
+//     Director: PropTypes.string.isRequired
+//   })
+// }
