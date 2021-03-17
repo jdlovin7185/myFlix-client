0f2f4dac2dda2d1a4df429444499f4e3b6fa3470
Josh Lovin <76003574+jdlovin7185@users.noreply.github.com>
2021-03-16 20:56:00 -0400

    skeloten for profile view

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,43 +1,132 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
 import './profile-view.scss';
 
-import {Card} from 'react-bootstrap';
+import {Card, Form} from 'react-bootstrap';
 
 export class ProfileView extends React.Component {
 
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      movies: [],
+      user: null,
+      email: null
+    };
+  }
+  componentDidMount() {
+    let accessToken = localStorage.getItem('token');
+    if (accessToken !== null) {
+      this.setState({
+        user: localStorage.getItem('user')
+      });
+      this.getUser(accessToken);
+    }
   }
 
+  getUser(token) {
+    axios.get('https://myflix1-0.herokuapp.com/users/${user}', {
+      headers: {Authorization: `Bearer ${token}`}
+    })
+    .then(response => {
+      this.setState({
+        Username: response.data.Username,
+        Password: response.data.Password,
+        Email: response.data.Email,
+        FavoriteMovies: response.data.FavoriteMovies
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  updateUser(user) {
+    axios.put('https://myflix1-0.herokuapp.com/users/:Username', {
+      headers: {Authorization: `Bearer ${token}`}
+    })
+    .then(response => {
+      this.setState({
+        Username: response.data.Username,
+        Password: response.data.Password,
+        Email: response.data.Email,
+        FavoriteMovies: response.data.FavoriteMovies
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  removeFav(movie) {
+    axios.delete('https://myflix1-0.herokuapp.com/users', {
+      headers: {Authorization: `Bearer ${token}`}
+    })
+    .then(response => {
+      this.setState
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  removeUser() {
+    axios.delete('https://myflix1-0.herokuapp.com/users/${user}', {
+      headers: {Authorization: `Bearer ${token}`}
+    })
+    .then(response => {
+      this.setState
+  })
+  .catch(function (error) {
+  console.log(error)
+  });
+}
+
   render() {
-    const { movies, users } = this.props;
-
-    if (!movies, !users) return <p>:')</p>;
-    // if (!movies) return null;
-
+    const {movies, users} = this.state;
     
+    // if (!register) return <RegistrationView onRegistered={register =>
+    //   this.onRegistered(register)} />;
+
+
+    // before the movies have been loaded
+    if (!users) return <div className="profile-view"/>;
+
     return (
       <div className="profile-view">
-        <Card>
-          <Card.Title>{movies.Director.name}</Card.Title>
+         <Card>
+          <Card.Title>Profile info</Card.Title>
           <Card.Body>
-            <Card.Text>{movies.Director.Bio}</Card.Text>
-            <Card.Text>Born: {movies.Director.Birth}</Card.Text>              
+            <Card.Text>{users.Username}</Card.Text>
+            <Card.Text>{users.Email}</Card.Text>
+            <Card.Text>{users.FavoriteMovie}</Card.Text>              
           </Card.Body>
         </Card>
+        {/* <Form className="updateInfo-form">
+      <h2>Want to update some info?</h2>
+      <Form.Group controlId="formGroupUsername">
+          <Form.Label>Username:</Form.Label>
+          <Form.Control type="username" value={username}
+      onChange={e => setUsername(e.target.value)}/>
+      </Form.Group>
+      <Form.Group controlId="formGroupPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" value={password}
+          onChange={e => setPassword(e.target.value)} />
+      </Form.Group>   
+      <Form.Group controlId="formGroupEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email sm" value={email}
+          onChange={e => setEmail(e.target.value)} />
+      </Form.Group>
+      <Button variant="primary" type="submit" 
+        onClick={handleSubmit}>
+          Submit
+      </Button>
+    </Form> */}
       </div>
     );
   }
-}
-
-ProfileView.propTypes = {
-  movies: PropTypes.shape({
-    Director: {
-      Name: PropTypes.string.isRequired,
-      Bio: PropTypes.string.isRequired
-    },
-  })
 }
